162ad73134c6723e237ac9360d13e0cc6bb1bd53
Azizo-B <aziz.baatout@gmail.com>
2024-08-21 17:20:27 +0200

    [FEATURE] algemene voorwaarden

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const profileIcon = document.getElementById("profile-icon");
-    profileIcon.addEventListener("click", function () {
-      const token = localStorage.getItem("authToken");
-      if (token) {
-        window.location.href = "/profile";
-      } else {
-        window.location.href = "/login";
-      }
-    });
+  const profileIcon = document.getElementById("profile-icon");
+  profileIcon.addEventListener("click", function () {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      window.location.href = "/profile";
+    } else {
+      window.location.href = "/login";
+    }
   });
+
+});
+function toggleFaqContent(button) {
+  const content = button.nextElementSibling;
+  const isOpen = button.classList.toggle("active");
+  content.style.display = isOpen ? "block" : "none";
+}
