9335a2ba16918760c6d566bfb858644fca3c7379
Azizo-B <aziz.baatout@gmail.com>
2024-08-27 21:23:30 +0200

    [FEATURE] ref tracking

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
+
   const profileIcon = document.getElementById("profile-icon");
+
   profileIcon.addEventListener("click", function () {
     const token = localStorage.getItem("authToken");
     if (token) {
@@ -9,7 +11,39 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  sendReferencePostRequest();
 });
+
+function collectTrackingInfo() {
+  return {
+    current_url: window.location.href,
+    referrer: document.referrer || 'Direct Access',
+    screen_width: window.screen.width,
+    screen_height: window.screen.height,
+  };
+}
+async function sendReferencePostRequest() {
+  const data = collectTrackingInfo();
+  try {
+    const response = await fetch('https://api.rijexamenmeldingen.be/ref-webhook', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      console.log('Success:', result);
+    } else {
+      console.error('Error:', response.status, response.statusText);
+    }
+  } catch (error) {
+    console.error('Error making request:', error);
+  }
+}
+
 function toggleFaqContent(button) {
   const content = button.nextElementSibling;
   const isOpen = button.classList.toggle("active");
